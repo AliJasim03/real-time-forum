443a942253c563dd0a3240a978f37d20cd11a105
afardan <lymfrdat@gmail.com>
2024-09-11 20:29:23 +0300

    added svg need to fix the placing

--- front-end/static/js/templates/user-list.js
+++ front-end/static/js/templates/user-list.js
@@ -34,11 +34,28 @@ export function populateOnlineUserList(users) {
         badgesContainer.className = 'd-flex align-items-center';
 
         // Create the exclamation mark badge, initially hidden
-        const typing = document.createElement('span');
-        typing.id = `user-typing-${user.ID}`;
-        typing.className = 'badge bg-warning text-dark ms-1 me-1'; // Margin left and right
-        typing.textContent = 'typing...';
-        typing.style.display = 'none'; // Initially hide the badge
+        // const typing = document.createElement('span');
+        // typing.id = `user-typing-${user.ID}`;
+        // typing.className = 'badge bg-warning text-dark ms-1 me-1'; // Margin left and right
+        // typing.textContent = 'typing...';
+        // typing.style.display = 'none'; // Initially hide the badge
+
+        const typingSVG = document.createElement('span');
+        typingSVG.id = `user-typing-${user.ID}`;
+        typingSVG.style.display = 'inline'; // Initially hide the SVG
+        typingSVG.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+                <circle cx="4" cy="12" r="3" fill="currentColor">
+                    <animate id="svgSpinners3DotsBounce0" attributeName="cy" begin="0;svgSpinners3DotsBounce1.end+0.25s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/>
+                </circle>
+                <circle cx="12" cy="12" r="3" fill="currentColor">
+                    <animate attributeName="cy" begin="svgSpinners3DotsBounce0.begin+0.1s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/>
+                </circle>
+                <circle cx="20" cy="12" r="3" fill="currentColor">
+                    <animate id="svgSpinners3DotsBounce1" attributeName="cy" begin="svgSpinners3DotsBounce0.begin+0.2s" calcMode="spline" dur="0.6s" keySplines=".33,.66,.66,1;.33,0,.66,.33" values="12;6;12"/>
+                </circle>
+            </svg>
+        `;
 
         // Create the exclamation mark badge, initially hidden
         const alertBadge = document.createElement('span');
@@ -54,7 +71,7 @@ export function populateOnlineUserList(users) {
         statusBadge.textContent = user.IsOnline ? 'Online' : 'Offline';
 
         // Append the badges to the container
-        badgesContainer.appendChild(typing);
+        badgesContainer.appendChild(typingSVG);
         badgesContainer.appendChild(alertBadge);
         badgesContainer.appendChild(statusBadge);
 
